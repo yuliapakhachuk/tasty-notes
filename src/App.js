59bd31edcb29ebe537ff7c5d2413ea39714e483f
Bohdan <yulia.pakhachuk@gmail.com>
2023-01-29 19:06:36 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Title } from './components/Title/Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title>HELLO YAMMMY BOOK</Title>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,7 @@
+
+import "./Title.scss";
+
+export const Title = (props) => {
+    // const {children} = this.props;
+    return <h2 className="title">{props.children}</h2>
+}
